4a741966341039209eb34989dc32e17690d4f94f
Arina Gladysheva <arina1110@mail.ru>
2023-01-27 22:12:34 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+    let res = '';
+    for (let i = String(n).length - 1; i >= 0; i--) {
+        if (String(n)[i] !== '-') {
+            res = res + String(n)[i];
+        }
+        
+    }
+    return Number(res);
+};
